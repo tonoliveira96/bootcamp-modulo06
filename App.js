ed316a760ba06c6f7b5ae87f80d3d01c2c71d7b1
Everton <ton.oliveira@sempre.facig.edu.br>
2020-03-19 21:21:07 -0300

    primeiro commit

---
+++ App.js
@@ -0,0 +1,29 @@
+
+import React, {Component}from 'react';
+import { Platform,StyleSheet, View,Text} from 'react-native';
+
+export default class App extends Component {
+  render(){
+    return (
+          <View style={styles.container}>
+            <Text style={styles.welcome}>Welcome to React</Text>
+            <Text style={styles.welcome}>Abaixo</Text>
+          </View>
+  );
+  }
+
+};
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5fcff',
+  },
+  welcome:{
+    fontSize: 20,
+    textAlign: 'center',
+    margin:0
+  }
+});
